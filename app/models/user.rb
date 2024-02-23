9dc97de3ee140bea021b364fa9777e834b2745a2
yamashun <yshunske@gmail.com>
2024-02-24 08:35:27 +0900

    set columns encrpt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  encrypts :email
+  encrypts :tel
 end
--- db/migrate/20240223233044_create_users.rb
+++ db/migrate/20240223233044_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :tel
+      t.string :email, limit: 510
+      t.string :tel, limit: 510
 
       t.timestamps
     end
